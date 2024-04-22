bb8c96c71d5c414cb5d192f0e28c3e7a5555cec4
Klas Mellbourn <klas@mellbourn.net>
2024-04-22 14:42:38 +0200

    adding types

--- packages/components/src/components/hello.ts
+++ packages/components/src/components/hello.ts
@@ -1,3 +1,5 @@
-export const hello = () => {
-  return "Hello from a component!";
+export type IHello = () => void;
+
+export const hello: IHello = () => {
+  return "Hello from a component 2!";
 };
--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./Header";
 export { type HeaderText } from "./Header";
 export { hello } from "./hello";
+export type { IHello } from "./hello";
