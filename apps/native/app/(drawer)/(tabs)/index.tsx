6de7ea04e86bfa2a190455745bac5a1f2cb5475d
Klas Mellbourn <klas@mellbourn.net>
2024-04-22 14:53:30 +0200

    IMPORTANT: do not "export type { IHello }", DO "export { type IHello}"

--- apps/native/app/(drawer)/(tabs)/index.tsx
+++ apps/native/app/(drawer)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import type { IHello } from '@npm-workspace-demo/components';
 import { hello } from '@npm-workspace-demo/components';
 import { Stack } from 'expo-router';
 
@@ -5,7 +6,8 @@ import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
-  const output = hello();
+  const myHello: IHello = hello; // try to use IHello here
+  const output = myHello();
   const title = 'Tab One: ' + output;
   return (
     <>
